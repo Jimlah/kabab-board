cd933325dfaba1b8de6f173dc49c39c0dc714961
Jimlah <46778885+Jimlah@users.noreply.github.com>
2024-04-06 18:31:52 +0100

    fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,35 @@
-import { type Config } from 'tailwindcss'
-
-export default {
-	content: [
-		'./resources/**/*.{js,ts,vue,blade.php}',
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/forms'),
-	],
-} satisfies Config
+import animate from "tailwindcss-animate"
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+        './resources/**/*.{ts,tsx,vue,php}'
+    ],
+    prefix: "",
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
+        extend: {
+            keyframes: {
+                "accordion-down": {
+                    from: { height: 0 },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: 0 },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
+        },
+    },
+    plugins: [animate],
+}
